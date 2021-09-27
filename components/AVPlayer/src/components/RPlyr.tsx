c29f88bb7660395da3df262fb3654e70f13a9d23
aFuzzyBear <drgaud@hotmail.com>
2021-09-27 01:43:43 +0100

    Able to pass props through now,

--- components/AVPlayer/src/components/RPlyr.tsx
+++ components/AVPlayer/src/components/RPlyr.tsx
@@ -1,16 +1,87 @@
-import react from 'react'
+import { Component  } from 'react'
 import ReactPlayer from 'react-player'
+import type ReactPlayerProps from 'react-player'
+// import { JsxItem } from 'astro/dist/types/@types/astro'
 
+interface SourceProps {
+    src: string
+    type: string
+  }
+export declare interface RPlyrProps{
+    url?: string | string[] | SourceProps[] | MediaStream
+    playing?: boolean
+    loop?: boolean
+    controls?: boolean
+    volume?: number
+    muted?: boolean
+    playbackRate?: number
+    width?: string | number
+    height?: string | number
+    style?: CSSStyleDeclaration
+    progressInterval?: number
+    playsinline?: boolean
+    playIcon?: JsxItem
+    previewTabIndex?: number
+    pip?: boolean
+    stopOnUnmount?: boolean
+    light?: boolean | string
+    fallback?: JsxItem
+    wrapper?: any
+    config?:any
+}
 
-const RPlayer = () => {
-
+const RPlyr = ({...props}:RPlyrProps):ReactPlayer => {
+    const {
+        url,
+        playing,
+        loop,
+        controls,
+        light,
+        volume,
+        muted,
+        playbackRate,
+        width,
+        height,
+        style,
+        progressInterval,
+        playsinline,
+        pip,
+        stopOnUnmount,
+        fallback,
+        wrapper,
+        playIcon,
+        previewTabIndex,
+        config
+    }=props
+    console.log(url)
     return(
         <div>
-            <ReactPlayer url='https://www.youtube.com/watch?v=dsTXcSeAZq8&ab_channel=Fireship' />
-
+            <ReactPlayer 
+                url = {url ? url : ReferenceError('Sorry, no URL was provided')} 
+                playing = {playing ?? playing}
+                loop = {loop ?? loop}
+                controls = {controls ?? controls}
+                light = {light ?? light}
+                volume = {volume ?? volume}
+                muted = {muted ?? muted}
+                playbackRate = {playbackRate ?? playbackRate}
+                width = {width ?? width}
+                height = {height ?? height}
+                style = {style ?? style}
+                progressInterval = {progressInterval ?? progressInterval}
+                playsinline= {playsinline ?? playsinline}
+                pip = {pip ?? pip}
+                stopOnUnmount = {stopOnUnmount ?? stopOnUnmount}
+                fallback = {fallback ?? fallback}
+                wrapper = {wrapper ?? wrapper} 
+                playIcon = {playIcon ?? playIcon}
+                previewTabIndex = {previewTabIndex ?? previewTabIndex}
+                config = {config ?? config}
+            />
+            
         </div>
 
     )
 }
 
-export default RPlayer
+export default RPlyr
