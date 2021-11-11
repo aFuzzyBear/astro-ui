5ffc9563cbeea2c654c005ec6a52674c7c225810
DrGaud <drgaud@hotmail.com>
2021-11-11 02:36:09 +0000

    Update index.js

--- components/XElement/index.js
+++ components/XElement/index.js
@@ -1,3 +1,23 @@
-import XElement from './XElement.astro'
+/**
+ *
+ *   __  __ _____  _                                _
+ *   \ \/ /| ____|| |  ___  _ __ ___    ___  _ __  | |_
+ *    \  / |  _|  | | / _ \| '_ ` _ \  / _ \| '_ \ | __|
+ *    /  \ | |___ | ||  __/| | | | | ||  __/| | | || |_
+ *   /_/\_\|_____||_| \___||_| |_| |_| \___||_| |_| \__|
+ *
+ * 			Web Component Generator For Astro
+ *
+ * @description XElement is a lightweight Web component generator for Astro
+ * which allows you to specify which HMTL compliant you wish to generate. It
+ * also allows for specialised delivery of JS to the Client, respecting
+ * Astro's Islands ethos.
+ *
+ * @copyright 2021
+ * @license GNU3 General Public License version 3
+ * @author jonathantneal - https://github.com/jonathantneal
+ * @author aFuzzyBear - https://github.com/aFuzzyBear
+ *
+ */
 
-export default XElement
+export {default as XElement} from './XElement.astro'
