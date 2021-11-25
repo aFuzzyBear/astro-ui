571a2b650b8149c8c74f3db812478ee8072daf89
aFuzzyBear <afuzzybear@outlook.com>
2021-11-25 00:49:22 +0000

    Added Artwork

--- components/Stylesheet/index.js
+++ components/Stylesheet/index.js
@@ -1,2 +1,33 @@
+/**
+ *                _          _               
+ *               / \    ___ | |_  _ __  ___  
+ *              / _ \  / __|| __|| '__|/ _ \ 
+ *             / ___ \ \__ \| |_ | |  | (_) |
+ *            /_/   \_\|___/ \__||_|   \___/ 
+ *                                           
+ *   ____   _           _             _                  _   
+ *  / ___| | |_  _   _ | |  ___  ___ | |__    ___   ___ | |_ 
+ *  \___ \ | __|| | | || | / _ \/ __|| '_ \  / _ \ / _ \| __|
+ *   ___) || |_ | |_| || ||  __/\__ \| | | ||  __/|  __/| |_ 
+ *  |____/  \__| \__, ||_| \___||___/|_| |_| \___| \___| \__|
+ *               |___/      
+ *              
+ *                      Astro Stylesheet 
+ * @description - Stylesheet Component generator 
+ * @author - aFuzzyBear - https://github.com/aFuzzyBear
+ * @copyright 2021
+ * @licence - MIT
+ * 
+ *      _     _   
+ *     (c).-.(c)  
+ *      / ._. \   
+ *    __\( Y )/__ 
+ *   (_.-/'-'\-._)
+ *      ||   ||   
+ *    _.' `-' '._ 
+ *   (.-./`-'\.-.)
+ *    `-'     `-' 
+ *  aFuzzyBear Production
+ */
 
 export {default as Stylesheet} from './Stylesheet.astro'
