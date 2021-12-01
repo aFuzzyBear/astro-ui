1f89c2187de74c50c5fc103b213d67c5fa5112f9
aFuzzyBear <afuzzybear@outlook.com>
2021-12-01 13:52:15 +0000

    working on styling atm

---
+++ www/xelement/postcss.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+};
---
+++ www/xelement/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.{scss,sass,css}', './src/**/*.{astro,js,jsx,ts,tsx,vue,scss,sass,css}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
