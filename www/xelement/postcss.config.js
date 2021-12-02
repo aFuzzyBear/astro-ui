475b664375dd221b0c84fa7afbdfd29ea6182c7d
aFuzzyBear <afuzzybear@outlook.com>
2021-12-02 19:58:35 +0000

    save

--- www/xelement/postcss.config.js
+++ www/xelement/postcss.config.js
@@ -3,8 +3,9 @@ const path = require('path');
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
+    require('postcss-preset-env')
   ]
 };
