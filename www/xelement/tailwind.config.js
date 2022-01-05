36e944db87d01ebe247fec82cb86e05c69dbbeaa
aFuzzyBear <afuzzybear@outlook.com>
2022-01-05 19:18:07 +0000

    Added prefetchlinks and navbar component

--- www/xelement/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.{scss,sass,css}', './src/**/*.{astro,js,jsx,ts,tsx,vue,scss,sass,css}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
