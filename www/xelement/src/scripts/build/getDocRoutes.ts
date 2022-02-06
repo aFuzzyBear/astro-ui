3ba2ad94d2c9b1e29baa8d0c31e106c286d94e06
aFuzzyBear <afuzzybear@outlook.com>
2022-02-06 15:03:21 +0000

    Created fixed routes

---
+++ www/xelement/src/scripts/build/getDocRoutes.ts
@@ -0,0 +1,27 @@
+
+const paths = Object.keys(import.meta.glob('../../../../../docs/XElement/**/*.md'))
+
+export function globby(){
+    const docdir = '../../../../../docs/XElement/'
+    return paths.map(
+        (slug) => {
+         return ({
+           params: {
+               slug: slug.slice(docdir.length,-3).split('/').slice(1).join('/').toLowerCase(),
+           },
+           props: {
+               slug: slug.slice(docdir.length,-3).split('/').slice(1).join('/').toLowerCase(),
+               directory: slug.slice(docdir.length,-3).split('/').slice(0,1).join('/').toLowerCase(),
+               path: new URL(slug, import.meta.url).pathname,
+           },
+       })}
+   ).filter(
+       x=>!(x.props.directory.includes('sarahedits'))
+       ).filter(
+           x=>!(x.props.directory.includes('readme'))
+           )
+}
+
+export default function filteredPaths(filterby:string){
+    return globby().filter(x=>x.props.directory.includes(filterby))
+}
