a3e9b9d0118c62e9d70a475023cdfc8cac12e515
aFuzzyBear <afuzzybear@outlook.com>
2022-02-13 10:52:32 +0000

    Added Build command to the file, 🤦‍♂️

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env bash
 
-# wget https://files.stork-search.net/releases/v1.4.0/stork-ubuntu-20-04
-# chmod +x stork-ubuntu-20-04
+
 curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
 chmod +x stork-amazon-linux
+./stork-amazon-linux build --input public/stork/stork.toml --output public/stork/xelement.st
