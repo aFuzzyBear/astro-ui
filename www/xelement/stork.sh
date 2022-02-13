528168152a1a465ead057722aae42d660185743d
aFuzzyBear <afuzzybear@outlook.com>
2022-02-13 15:12:51 +0000

    🤞🤞🤞

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -4,6 +4,6 @@ curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
 chmod +x stork-amazon-linux
 astro build --verbose
 echo "starting stork build"
-echo "$(<dist/res/stork/stork.toml)"
-#./stork-amazon-linux build --input dist/res/stork/stork.toml --output dist/res/stork/xelement.st
+# echo "$(<dist/res/stork/stork.toml)"
+sudo ./stork-amazon-linux build --input dist/res/stork/stork.toml --output dist/res/stork/xelement.st
 echo "stork build finished"
