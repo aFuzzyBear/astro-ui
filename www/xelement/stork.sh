3b4f0ad02f0250f1afa2d21b17a31bd760880c01
aFuzzyBear <afuzzybear@outlook.com>
2022-02-13 00:33:02 +0000

    fixed path to toml

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -2,4 +2,4 @@
 
 wget https://files.stork-search.net/releases/v1.4.0/stork-ubuntu-20-04
 chmod +x stork-ubuntu-20-04
-./stork-ubuntu-20-04 build --input public/stork/Stork.toml --output public/stork/xelement.st
+./stork-ubuntu-20-04 build --input public/stork/stork.toml --output public/stork/xelement.st
