0925533d90532efef7350a525a8261b4385e7275
aFuzzyBear <afuzzybear@outlook.com>
2022-02-13 10:41:45 +0000

    With Help from @lostra01,  resolving the stork cli issue

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -2,6 +2,5 @@
 
 # wget https://files.stork-search.net/releases/v1.4.0/stork-ubuntu-20-04
 # chmod +x stork-ubuntu-20-04
-wget https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
+curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
 chmod +x stork-amazon-linux
-./stork-ubuntu-20-04 build --input public/stork/stork.toml --output public/stork/xelement.st
