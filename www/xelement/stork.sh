ebb32333bd90fa2bab53172c3ed0c9fc64d1b053
lostra01 <lostra01@gmail.com>
2022-02-13 14:56:27 +0100

    re-writing build pipeline

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env bash
 
-
-#curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
-#chmod +x stork-amazon-linux
-#./stork-amazon-linux build --input public/stork/stork.toml --output public/stork/xelement.st
+curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
+chmod +x stork-amazon-linux
+astro build --verbose
+./stork-amazon-linux build --input dist/stork/stork.toml --output dist/stork/xelement.st
