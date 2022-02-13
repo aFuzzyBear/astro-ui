bdb18be1eb4faa85ebcf8a8d8d1153345c262fbf
lostra01 <lostra01@gmail.com>
2022-02-13 15:11:22 +0100

    testing verbose in stork?

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -4,5 +4,5 @@ curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
 chmod +x stork-amazon-linux
 astro build --verbose
 echo "starting stork build"
-./stork-amazon-linux build --input dist/res/stork/stork.toml --output dist/res/stork/xelement.st
+./stork-amazon-linux build --input dist/res/stork/stork.toml --output dist/res/stork/xelement.st --verbose
 echo "stork build finished"
