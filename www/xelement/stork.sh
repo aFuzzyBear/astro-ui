9688d426b29abc408714ba8a7bdc8bb4d5d76c0e
aFuzzyBear <afuzzybear@outlook.com>
2022-02-13 19:09:16 +0000

    Added contents field to the toml -expect huge toml file now

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -6,8 +6,5 @@ astro build --verbose
 echo "starting stork build"
 echo "$(<public/res/stork/stork.toml)"
 echo "added xelement.st file"
-touch /xelement.st
-chmod 777 /xelement.st
 ./stork-amazon-linux build --input public/res/stork/stork.toml --output - > /xelement.st
-ls -l
 echo "stork build finished"
