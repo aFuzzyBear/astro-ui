3643d46c393e988ec53b4ddcde151cb7937dbf02
lostra01 <lostra01@gmail.com>
2022-02-13 13:01:48 +0100

    moved stork handling entirely into search.astro

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env bash
 
 
-curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
-chmod +x stork-amazon-linux
+#curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
+#chmod +x stork-amazon-linux
 #./stork-amazon-linux build --input public/stork/stork.toml --output public/stork/xelement.st
