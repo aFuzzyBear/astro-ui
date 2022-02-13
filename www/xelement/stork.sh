8a3bb499196b252f131c346da43314ff4d8a1560
lostra01 <lostra01@gmail.com>
2022-02-13 18:59:04 +0100

    test fixing dir

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -5,8 +5,6 @@ chmod +x stork-amazon-linux
 astro build --verbose
 echo "starting stork build"
 # echo "$(<dist/res/stork/stork.toml)"
-touch dist/res/stork/xelement.st
-chmod 777 dist/res/stork/xelement.st
 echo "added xelement.st file"
-./stork-amazon-linux build --input dist/res/stork/stork.toml --output - > dist/res/stork/xelement.st
+./stork-amazon-linux build --input public/res/stork/stork.toml --output - > dist/res/stork/xelement.st
 echo "stork build finished"
