1d625f7b03d75ae9dcdc59c59bc98e9cb1da38b3
lostra01 <lostra01@gmail.com>
2022-02-13 12:23:38 +0100

    removing stork run from bash script

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -3,4 +3,4 @@
 
 curl -O https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
 chmod +x stork-amazon-linux
-./stork-amazon-linux build --input public/stork/stork.toml --output public/stork/xelement.st
+#./stork-amazon-linux build --input public/stork/stork.toml --output public/stork/xelement.st
