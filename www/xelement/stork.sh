805b2bb3aec2e8da60c6cdbfe09f7c4fd9c41491
aFuzzyBear <afuzzybear@outlook.com>
2022-02-13 01:57:08 +0000

    Still trying

--- www/xelement/stork.sh
+++ www/xelement/stork.sh
@@ -1,5 +1,7 @@
 #!/user/bin/env bash
 
-wget https://files.stork-search.net/releases/v1.4.0/stork-ubuntu-20-04
-chmod +x stork-ubuntu-20-04
+# wget https://files.stork-search.net/releases/v1.4.0/stork-ubuntu-20-04
+# chmod +x stork-ubuntu-20-04
+wget https://files.stork-search.net/releases/v1.4.0/stork-amazon-linux
+chmod +x stork-amazon-linux
 ./stork-ubuntu-20-04 build --input public/stork/stork.toml --output public/stork/xelement.st
