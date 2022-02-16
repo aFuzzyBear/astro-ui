9a760f4054ab219718daef49209bb197f57bb662
aFuzzyBear <afuzzybear@outlook.com>
2022-02-16 01:00:59 +0000

    removed flexbox and applied the GRID

---
+++ components/BeforeAfter/index.js
@@ -0,0 +1,35 @@
+'use strict'
+/**
+ *                _          _               
+ *               / \    ___ | |_  _ __  ___  
+ *              / _ \  / __|| __|| '__|/ _ \ 
+ *             / ___ \ \__ \| |_ | |  | (_) |
+ *            /_/   \_\|___/ \__||_|   \___/ 
+ *                                           
+ *
+ *                  _____           _   
+ *                 |  ___|__  _ __ | |_ 
+ *                 | |_ / _ \| '_ \| __|
+ *                 |  _| (_) | | | | |_ 
+ *                 |_|  \___/|_| |_|\__|
+ *                          
+ *              
+ *                      Astro Font Component 
+ * @description - A Specialised Font component which utilise the 'google-font-API' to obtain font-files and other relevant information and applies them into the project 
+ * @author - Lostra01 - https://github.com/lostra01
+ * @author - aFuzzyBear - https://github.com/aFuzzyBear
+ * @copyright 2022
+ * @licence - MIT
+ * 
+ *      _     _   
+ *     (c).-.(c)  
+ *      / ._. \   
+ *    __\( Y )/__ 
+ *   (_.-/'-'\-._)
+ *      ||   ||   
+ *    _.' `-' '._ 
+ *   (.-./`-'\.-.)
+ *    `-'     `-' 
+ *  aFuzzyBear Production
+ */
+export {default as BeforeAfter }from './BeforeAfter.astro'
